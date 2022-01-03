32e4758b184a1076eec913f9b3af782a3f72009d
Elena Stefanovska <64511693+Elijana0@users.noreply.github.com>
2022-01-03 21:54:49 +0100

    Add files via upload

---
+++ New/years.js
@@ -0,0 +1,12 @@
+
+function calculateAge(){
+    let numA = parseInt(prompt("Enter your birth year"));
+    let numB = parseInt(prompt("Enter this year"));
+   
+    let age = (numB - numA)-1;
+    return age;
+}
+ 
+ console.log(`You are ${calculateAge()} years old`);
+ console.log(`You are ${calculateAge()} years old`);
+ console.log(`You are ${calculateAge()} years old`);
