d45bf533c0702798f733d697f34c15bf88850b91
Elena Stefanovska <64511693+Elijana0@users.noreply.github.com>
2022-01-09 19:47:30 +0100

    Add files via upload

---
+++ string.js
@@ -0,0 +1,10 @@
+let arr = ["Hello", "there", "students", "of", "SEDC", "!"];
+let array = ['You', 'guys', 'are', 'great', '!'];
+
+function strings() {
+    console.log(array.join(' '));
+    console.log(arr.join(' '));
+    }
+
+console.log(strings());
+alert(array.join(' '));
