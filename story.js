d023fa16edd9184c71e4207db26f2462b92c94b4
Elena Stefanovska <64511693+Elijana0@users.noreply.github.com>
2022-01-09 19:49:49 +0100

    Delete story.js

--- story.js
+++
@@ -1,8 +0,0 @@
-let arr = ['name', 'mood', 'acctivity'];
-
-function tellStory(){
-    let arr = ['Jane', 'happy', 'reading'];
-    return `This is ${arr[0]}. ${arr[0]} is a nice person. Today she is ${arr[1]}. She is ${arr[2]} all day. The end.`;
-}
-console.log(tellStory());
-alert(tellStory());
